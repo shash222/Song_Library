83523cf39f1a3e7c5b620c7d8b4eec0e163d9978
Salman Hashmi <shash222@yahoo.com>
2019-09-24 14:17:00 -0400

    Successfully populate text fields with selected song data and show/hide song details

--- src/Controller/SongLib.java
+++ src/Controller/SongLib.java
@@ -1,26 +1,66 @@
 package Controller;
 
 import Model.Song;
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
 
 public class SongLib {
-    private static List<Song> testSongs = new ArrayList<>();
-    private static List<Song> testSongs2 = new ArrayList<>();
+    @FXML
+    private TextField song;
+    @FXML
+    private TextField artist;
+    @FXML
+    private TextField album;
+    @FXML
+    private TextField year;
+    @FXML
+    private Label albumLabel;
+    @FXML
+    private Label yearLabel;
+    @FXML
+    private ListView<String> songList;
+    @FXML
+    private CheckBox showDetails;
 
-    private static void testSetup() {
+
+    private List<String> testSongs = new ArrayList<>();
+    private Map<String, Song> songNameToPOJOMap = new HashMap<>();
+
+    @FXML
+    private void testSetup(ActionEvent event) {
         for (int i = 20; i >= 1; i--) {
-            testSongs.add(new Song("Song" + i, "Arist" + i, "Album" + i, i));
+            String songIdentifier = String.format("Song%d - Artist%d", i, i);
+            Song song = new Song("Song" + i, "Arist" + i, "Album" + i, i);
+            testSongs.add(songIdentifier);
+            songNameToPOJOMap.put(songIdentifier.toLowerCase(), song);
         }
-        for (int i = 20; i >= 1; i--) {
-            testSongs2.add(new Song("Song", "Arist" + i, "Album" + i, i));
-        }
+        sortList(testSongs);
+        songList.setItems(FXCollections.observableList(testSongs));
     }
 
-    private static void printList(List<Song> list) {
+    private void printList(List<Song> list) {
         list.forEach(s -> {
             System.out.println(s.getSongName());
             System.out.println(s.getArtist());
@@ -30,20 +70,49 @@ public class SongLib {
         });
     }
 
-    private static void sortList(List<Song> list) {
-        Collections.sort(list, new Comparator<Song>() {
-            @Override
-            public int compare(Song o1, Song o2) {
-                int songNameComparisonValue = o1.getSongName().compareToIgnoreCase(o2.getSongName());
-                return (songNameComparisonValue == 0)
-                        ? o1.getArtist().compareToIgnoreCase(o2.getArtist())
-                        : songNameComparisonValue;
-            }
+    private void sortList(List<String> list) {
+        // Lambda expression takes place of instantiating new Comparator, which is defined between the brackets
+        Collections.sort(list, (o1, o2) -> {
+            String[] splitSongIdentifier1 = o1.split("-");
+            String[] splitSongIdentifier2 = o2.split("-");
+            int songNameComparisonValue = splitSongIdentifier1[0].compareToIgnoreCase(splitSongIdentifier2[0]);
+            return (songNameComparisonValue == 0)
+                    ? splitSongIdentifier1[1].compareToIgnoreCase(splitSongIdentifier2[1])
+                    : songNameComparisonValue;
         });
     }
 
-//    public static void main(String[] args) {
-//        testSetup();
-//
-//    }
+    @FXML
+    private void onSongSelection(MouseEvent event) {
+        try {
+            String songEntry = songList.getSelectionModel().getSelectedItem();
+            Song selectedSong = songNameToPOJOMap.get(songEntry.toLowerCase());
+            song.setText(selectedSong.getSongName());
+            artist.setText(selectedSong.getArtist());
+            album.setText(selectedSong.getAlbum());
+            year.setText(Integer.toString(selectedSong.getYear()));
+        } catch(Exception e) {
+
+        }
+    }
+
+    /**
+     * Shows song details when CheckBox is marked
+     * @param event ActionEvent parameter is required to link fxml to Java code
+     */
+    @FXML
+    private void showSongDetails(ActionEvent event) {
+        if (showDetails.isSelected()) {
+            album.setVisible(true);
+            year.setVisible(true);
+            albumLabel.setVisible(true);
+            yearLabel.setVisible(true);
+        } else {
+            album.setVisible(false);
+            year.setVisible(false);
+            albumLabel.setVisible(false);
+            yearLabel.setVisible(false);
+        }
+    }
+
 }
